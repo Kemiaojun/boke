683ef295b9e606dd4f1509b3d78ac0941d07ccaf
克喵 <kemiaofx@gmail.com>
2024-11-08 00:16:52 +0800

    Site updated: 2024-11-08 00:16:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/07/hello-world/","2024/11/07/ceshi/"];function toRandomPost(){
+var posts=["2024/11/07/ceshi/","2024/11/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
